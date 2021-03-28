d526bf5edfb83d19e0c97062e3ac7f995f1d3d4f
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-27 23:06:16 -0400

    playing around with faker

--- demo/app/controllers/home_controller.rb
+++ demo/app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
-  def index
-    @name = "John Smith"
+  before_action :common, :only => [:index, :team]
+  def common
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @hero_description = Faker::Superhero.descriptor
+    @hero_prefix = Faker::Superhero.prefix
+    @hero_suffix = Faker::Superhero.suffix
+    @avatar = Faker::Avatar.image
+    @hero_dayjob = Faker::Job.field
+    @hero_realname = Faker::Name.first_name + ' ' + Faker::Name.last_name
+    @hero_archenemy = Faker::Ancient.hero
+  end
+  
+  def index 
   end
 
   def team
