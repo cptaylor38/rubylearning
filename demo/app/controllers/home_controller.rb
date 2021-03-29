575182779e0e40f5935889f19d111166e75178b7
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-29 03:09:37 -0400

    modified assignment to use class, note - use locals for shared partial rendering

--- demo/app/controllers/home_controller.rb
+++ demo/app/controllers/home_controller.rb
@@ -1,17 +1,19 @@
 class HomeController < ApplicationController
   before_action :common, :only => [:index, :team]
   def common
-    @hero_name = Faker::Superhero.name
-    @hero_power = Faker::Superhero.power
-    @hero_description = Faker::Superhero.descriptor
-    @hero_prefix = Faker::Superhero.prefix
-    @hero_suffix = Faker::Superhero.suffix
-    @avatar = Faker::Avatar.image
-    @hero_dayjob = Faker::Job.field
-    @hero_realname = Faker::Name.first_name + ' ' + Faker::Name.last_name
-    @hero_archenemy = Faker::Ancient.hero
-    @hero_hobby = Faker::Company.bs.titleize
-    @hero_age = rand(1..1000)
+    @hero = Hero.new(
+      Faker::Superhero.name,
+      Faker::Superhero.power,
+      Faker::Superhero.descriptor,
+      Faker::Superhero.prefix,
+      Faker::Superhero.suffix,
+      Faker::Avatar.image,
+      Faker::Job.field,
+      Faker::Name.first_name + ' ' + Faker::Name.last_name,
+      Faker::Ancient.hero,
+      Faker::Company.bs.titleize,
+      rand(1..1000)
+    )
   end
   
   def index 
@@ -19,5 +21,41 @@ class HomeController < ApplicationController
 
   def team
     @team_number = params[:team_number].to_i
+    @team_array = []
+    
+    @team_number.times do
+      new_hero = Hero.new(
+        Faker::Superhero.name,
+        Faker::Superhero.power,
+        Faker::Superhero.descriptor,
+        Faker::Superhero.prefix,
+        Faker::Superhero.suffix,
+        Faker::Avatar.image,
+        Faker::Job.field,
+        Faker::Name.first_name + ' ' + Faker::Name.last_name,
+        Faker::Ancient.hero,
+        Faker::Company.bs.titleize,
+        rand(1..1000)
+      )
+      @team_array.push(new_hero)
+    end
+  end
+
+end
+
+class Hero 
+  attr_accessor :name, :power, :description, :prefix, :suffix, :image, :dayjob, :realname, :nemesis, :hobby, :age
+  def initialize( name, power, description, prefix, suffix, image, dayjob, realname, nemesis, hobby, age  )
+    self.name         = name
+    self.power        = power
+    self.description  = description
+    self.prefix       = prefix
+    self.suffix       = suffix
+    self.image        = image
+    self.dayjob       = dayjob
+    self.realname     = realname
+    self.nemesis      = nemesis
+    self.hobby        = hobby
+    self.age          = age
   end
 end
